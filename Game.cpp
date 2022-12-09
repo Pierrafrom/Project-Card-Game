da78ba3c7f2cdd7d38d84f54d8a980819d5a35d4
Pierre Fromont Boissel <118841244+Pierrafrom@users.noreply.github.com>
2022-12-10 00:23:03 +0100

    Add ended method

--- Game.cpp
+++ Game.cpp
@@ -1,25 +1,52 @@
+// Project Card Game
+// by Pierre
+// and Samuel
+
 #include <vector>
-#include <iostream>
 #include "Game.h"
+#include "Player.h"
+#include "Card.h"
 
 using namespace std;
 
-
-Game::Game() {
-    _reserve={};
-    _player1("name", 0, cards);
-    _player2("name", 0, cards);
+// Empty constructor
+Game::Game()
+{
+    _reserve = {};
+    _player1;
+    _player2;
 }
 
-Game::Game(vector<Card> reserve, Player player1, Player player2) {
+// Constructor
+Game::Game(const vector<Card> &reserve, Player &player1, Player &player2)
+{
     _reserve = reserve;
     _player1 = player1;
     _player2 = player2;
 }
 
-bool Game::ended(Player & winner){
-    if (0 == winner.GetPrestige()) ///demander clarification au prof question 3 classe Jeu
+// Know if the game is finished and give the winner
+bool Game::ended(Player &winner)
+{
+    bool finish = !_player1.enoughCards() || !_player2.enoughCards() || _player1.GetPrestige() <= 0
+                  || _player2.GetPrestige() <= 0;
+    if (finish)
     {
-        return true;
+        if (_player1.GetPrestige() > _player2.GetPrestige())
+        {
+            winner = _player1;
+        }
+        else
+        {
+            if (_player1.GetPrestige() < _player2.GetPrestige())
+            {
+                winner = _player2;
+            }
+            else
+            {
+                throw out_of_range("Draw : no winner");
+            }
+        }
     }
+    return finish;
 }

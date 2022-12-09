d82dd568acf8b64678818cfe18b9b5c2bd241e72
Pierre Fromont Boissel <118841244+Pierrafrom@users.noreply.github.com>
2022-12-09 12:15:41 +0100

    add method playsACard

--- Player.cpp
+++ Player.cpp
@@ -5,47 +5,53 @@
 using namespace std;
 
 ///default constructor
-Player::Player(){
+Player::Player()
+{
     _name = "Yugi";
     _prestige = 25;
-    Card righthand("RightHand",1,0,0);
-    Card lefthand("LeftHand",0,1,0);
-    Card head("Head",1,-2,0);
-    Card mouth("Mouth",1,-1,2);
-    Card knee("Knee",2,0,0);
-    _cards = {righthand,lefthand,head,mouth,knee};
+    Card righthand("RightHand", 1, 0, 0);
+    Card lefthand("LeftHand", 0, 1, 0);
+    Card head("Head", 1, -2, 0);
+    Card mouth("Mouth", 1, -1, 2);
+    Card knee("Knee", 2, 0, 0);
+    _deck = {righthand, lefthand, head, mouth, knee};
 }
 
 
 ///constructor
-Player::Player(string name, int prestige, vector<Card> cards){
+Player::Player(string name, int prestige, vector<Card> cards)
+{
     _name = name;
     _prestige = prestige;
-    _cards = cards;
+    _deck = cards;
 }
 
 
 ///get the name of targeted player
-string Player::GetName() const{
+string Player::GetName() const
+{
     return _name;
 }
 
 
 ///get the number prestige points of targeted player
-int Player::GetPrestige() const {
+int Player::GetPrestige() const
+{
     return _prestige;
 }
 
 ///Displaying the information of the targeted player
-void Player::display(){
-    cout<<"The player "<<_name<<" has "<<_prestige<<" prestige points, only "<<_cards.size()<<" left in his pile ⚡"<<endl;
+void Player::display()
+{
+    cout << "The player " << _name << " has " << _prestige << " prestige points, only " << _deck.size()
+         << " left in his pile ⚡" << endl;
 }
 
 void Player::init(vector<Card> &reserve)
 {
     cout << "initialisation of the player's deck" << endl;
     int size = reserve.size();
-    if (size >= 20) // est ce que les cartes piochés sont encore dans le reserve
+    if (size >= 20)
     {
         for (int i = 0; i <= 20; i++)
         {
@@ -54,21 +60,48 @@ void Player::init(vector<Card> &reserve)
     }
 }
 
-bool Player::enough_cards(){
-    return (_prestige<=0);
+bool Player::enoughCards()
+{
+    return (_prestige >= 0);
 }
 
-void Player::next_card(Card &card)
+void Player::nextCard(Card &card)
 {
     int size = _deck.size();
-    if (size>0)
+    if (size > 0)
     {
-        card = _deck[size-1];
+        card = _deck[size - 1];
         _deck.pop_back();
     }
     else
     {
         throw out_of_range("no more cards left !");
+    }
+}
+
+// update prestige after a play
+void Player::playsACard(Player &player)
+{
+    Card c, c1;
+    this->nextCard(c);
+    player.nextCard(c1);
+    int resPhysicalMatch = c + c1;
+    int resMagicalMatch = c ^ c1;
+    if (resPhysicalMatch > 0)
+    {
+        _prestige -= resPhysicalMatch;
+    }
+    else
+    {
+        player._prestige += resPhysicalMatch;
+    }
+    if (resMagicalMatch > 0)
+    {
+        _prestige -= resMagicalMatch;
+    }
+    else
+    {
+        player._prestige += resMagicalMatch;
     }
 }
 

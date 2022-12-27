f6b2deb46d209550abae68a0541903ee4a3fb6ab
Pierre Fromont Boissel <118841244+Pierrafrom@users.noreply.github.com>
2022-12-27 17:34:12 +0100

    Delete main_test.h

--- main_test.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by samue on 23/12/2022.
-//
-
-#ifndef PROJECT_CARD_GAME_MAIN_MAIN_TEST_H
-#define PROJECT_CARD_GAME_MAIN_MAIN_TEST_H
-void main_test();
-#endif //PROJECT_CARD_GAME_MAIN_MAIN_TEST_H

577bebb77d411ef799352e4e2490584e2a89a483
Samuel <119617502+Samuelito78@users.noreply.github.com>
2023-01-05 20:11:20 +0100

    Update Card.cpp

--- Card.cpp
+++ Card.cpp
@@ -2,197 +2,206 @@
 // by Pierre
 // and Samuel
 
-#include "Card.h"
-#include <iostream>
+#include <vector>
 #include <fstream>
 #include <string>
 #include <sstream>
+#include "Game.h"
+#include "Player.h"
+#include "Card.h"
 
 using namespace std;
 
 // Empty constructor
-Card::Card()
+Game::Game()
 {
-    _name = "bare hands";
-    _attack = 1;
-    _defense = 1;
-    _magic = 0;
+    _reserve = {};
+    _player1;
+    _player2;
 }
 
 // Constructor
-Card::Card(const string &name, int attack, int defense, int magic)
+Game::Game(const vector<Card> &reserve, Player &player1, Player &player2)
 {
-    _name = name;
-    _attack = attack;
-    _defense = defense;
-    _magic = magic;
+    _reserve = reserve;
+    _player1 = player1;
+    _player2 = player2;
 }
 
-// Display long
-void Card::displayLong() const
+// Know if the game is finished and give the winner
+bool Game::ended(Player &winner) const
 {
-    cout << "****************************" << endl;
-    cout << "name: " << _name << endl;
-    cout << "attack: " << _attack << endl;
-    cout << "defense: " << _defense << endl;
-    cout << "magic: " << _magic << endl;
-    cout << "****************************" << endl;
-}
-
-// Display short
-void Card::displayShort() const
-{
-    cout << _name << " [ " << _attack << " ; " << _defense << " ; " << _magic << " ] " << endl;
-}
-
-// Physical damages
-int Card::physicalDamage(const Card &card) const
-{
-    if (_attack > card._attack)
+    bool finish = !_player1.enoughCards() || !_player2.enoughCards() || _player1.GetPrestige() <= 0
+                  || _player2.GetPrestige() <= 0;
+    if (finish)
     {
-        if (_attack < card._defense)
+        if (_player1.GetPrestige() > _player2.GetPrestige())
         {
-            return 0;
+            winner = _player1;
         }
         else
         {
-            return -(_attack - card._defense);
-        }
-    }
-    else
-    {
-        if (card._attack == _attack)
-        {
-            return 0;
-        }
-        else
-        {
-            if (card._attack > _defense)
+            if (_player1.GetPrestige() < _player2.GetPrestige())
             {
-                return (card._attack - _defense);
+                winner = _player2;
             }
             else
             {
-                return 0;
+                throw out_of_range("Draw : no winner");
             }
         }
     }
+    return finish;
 }
 
-// Magical damages
-int Card::magicalDamage(const Card &card) const
+int Game::winner()
 {
-    if (_magic == card._magic)
-    {
-        return 0;
+    Player winner;
+    if (this->ended(winner)){
+        if (winner==_player1)
+        {
+            return 1;
+        }
+        if (winner==_player2)
+        {
+            return -1;
+        }
     }
-    else
-    {
-        return (card._magic - _magic);
-    }
+    return 0;
 }
 
-// operator + for physicalDamage
-int Card::operator+(const Card &card) const
+// plays one round of the game
+void Game::operator++()
 {
-    return physicalDamage(card);
+    _player1.playsACard(_player2, _reserve);
 }
 
-// operator ^ for magicalDamage
-int Card::operator^(const Card &card) const
+// return true if the game is finished
+bool Game::IsGameFinished() const
 {
-    return magicalDamage(card);
-}
-
-// operator << for display short
-void operator<<(ostream &os, const Card &card)
-{
-    card.displayShort();
-}
-
-//operator for equality
-bool Card::operator==(const Card &card) const
-{
-    return (this->_name==card._name && this->_attack==card._attack &&
-    this->_defense==card._defense && this->_magic==card._magic);
+    return !(_player1.enoughCards() && _player1.enoughPrestige() && _player2.enoughCards() && _player2.enoughPrestige());
 }
 
 // The following methods are not in the instructions, but we had them because they are useful for tests
 
-// Get the Name
-string Card::getName() const
+// compare game
+bool Game::compareGame(const Game &game)
 {
-    return _name;
+    return this->compareReserve(game._reserve) &&
+            ((_player1 == game._player1 && _player2 == game._player2) ||
+            (_player1 == game._player2 && _player2 == game._player1));
 }
 
-// Get the attack
-int Card::getAttack() const
+// Modify the prestige of the player two
+void Game::modifyPlayerTwoPrestige(int num)
 {
-    return _attack;
+    _player2.modifyPrestige(num);
 }
 
-// Get the defense
-int Card::getDefense() const
+// Clear the deck of the player Two
+void Game::clearPlayerTwoDeck()
 {
-    return _defense;
+    _player2.clearDeck();
 }
 
-// Get the magic
-int Card::getMagic() const
+// Compare the reserve of card
+bool Game::compareReserve(const vector<Card> &vector) const
 {
-    return _magic;
+    bool ok = true;
+    int size = _reserve.size();
+    if (vector.size() != size)
+    {
+        ok = false;
+    }
+    if (ok)
+    {
+        int i = 0;
+        while (ok && i < size)
+        {
+            if (!(_reserve[i] == vector[i]))
+            {
+                ok = false;
+            }
+            i++;
+        }
+    }
+    return ok;
 }
 
-void Card::fill(const string &line) {
-    // opening the file
+// Get the player 1
+Player Game::GetPlayerOne()
+{
+    return _player1;
+}
+
+// Get the player 2
+Player Game::GetPlayerTwo()
+{
+    return _player2;
+}
+
+//useful for tests only
+void Game::getReserve() {
+    for(int i = 0;i<_reserve.size();i++){
+        _reserve[i].displayShort();
+        cout<<endl;
+    }
+}
+
+bool compareCards(Card obj1, Card obj2)  {
+    return obj1.getName() < obj2.getName();
+}
+
+void Game::fillReserve(const string &filename) {
     ifstream file;
-    file.open("C:/Users/samue/Project-Card-Game-main/cards_data.txt");
+    file.open(filename);
 
     if (!file.is_open()) {
         cout << "Error while opening the file" << endl;
     }
     else {
-        string actual_line;
+        string actual_line, object_name, name;
+        int attack, defense, magic;
+        int i = 0;
+        while (i<_reserve.size()){
+            getline(file, actual_line);
+            stringstream stream(actual_line);
+            string value;
 
-        while (getline(file, actual_line)) {
+            getline(stream, value, ',');
+            istringstream(value) >> name;
 
-            if(actual_line==line) {
-                // declaration of a string flow in the beginning of each line
-                istringstream stream(actual_line);
+            getline(stream, value, ',');
+            istringstream(value) >> attack;
 
-                // declaration of a string for the first part of the line
-                string part1;
-/*
-                // declaration of an int for the second part of each line
-                int part2;
+            getline(stream, value, ',');
+            istringstream(value) >> defense;
 
-                // declaration of an int for the third part of each line
-                int part3;
-
-                // declaration of an int for the fourth part of each line
-                int part4 ;*/
-
-                getline(stream, part1, ',');
+            getline(stream, value, ',');
+            istringstream(value) >> magic;
 
 
-                // takes the part 1 of the line (string type)
-
-                _name = part1;
-
-                // takes the part 2 of the line (int type)
-                stream >> _attack;
-
-
-                // takes the part 3 of the line (int type)
-                stream >> _defense;
-
-
-                // takes the part 4 of the line (int type)
-                stream >> _magic;
-            }
-
+            _reserve[i].changeName(name);
+            _reserve[i].changeAttack(attack);
+            _reserve[i].changeDefense(defense);
+            _reserve[i].changeMagic(magic);
+            i++;
         }
     }
+    sort(_reserve);
     file.close();
-
 }
+
+void Game::sort(vector<Card> &reserve) {
+    for (int i = 0; i < reserve.size(); i++) {
+        for (int j = i+1; j < reserve.size(); j++) {
+            if (compareCards(reserve[j], reserve[i])) {
+                swap(reserve[i], reserve[j]);
+            }
+        }
+    }
+}
+
+
+
+

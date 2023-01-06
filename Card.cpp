76e778b7fa1a2f07fe766c3804e66459210f76cb
Pierre Fromont Boissel <118841244+Pierrafrom@users.noreply.github.com>
2023-01-07 00:53:15 +0100

    Update Card.cpp

--- Card.cpp
+++ Card.cpp
@@ -82,14 +82,7 @@ int Card::physicalDamage(const Card &card) const
 // Magical damages
 int Card::magicalDamage(const Card &card) const
 {
-    if (_magic == card._magic)
-    {
-        return 0;
-    }
-    else
-    {
-        return (card._magic - _magic);
-    }
+    return (card._magic - _magic);
 }
 
 // operator + for physicalDamage
@@ -110,13 +103,544 @@ void operator<<(ostream &os, const Card &card)
     card.displayShort();
 }
 
-
-
 //operator for equality
 bool Card::operator==(const Card &card) const
 {
-    return (this->_name==card._name && this->_attack==card._attack &&
-    this->_defense==card._defense && this->_magic==card._magic);
+    return (this->_name == card._name && this->_attack == card._attack &&
+            this->_defense == card._defense && this->_magic == card._magic);
+}
+
+// compare with Attack then with magic then with defense
+bool Card::compareCard(const Card &card, Tri sortCriteria) const
+{
+    // We don't have to put break because we return before reach it
+    switch (sortCriteria)
+    {
+        case (ScoreWithCoefAMDDesc):
+            return (this->getScoreAMD() > card.getScoreAMD());
+        case (ScoreWithCoefAMDAsc):
+            return (this->getScoreAMD() < card.getScoreAMD());
+        case (ScoreDesc):
+            return (this->getScore() > card.getScore());
+        case (ScoreAsc):
+            return (this->getScore() < card.getScore());
+        case (DMAAsc):
+            if (_defense < card._defense)
+            {
+                return true;
+            }
+            else
+            {
+                if (_defense > card._defense)
+                {
+                    return false;
+                }
+                else
+                {
+                    if (_magic < card._magic)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        if (_magic > card._magic)
+                        {
+                            return false;
+                        }
+                        else
+                        {
+                            if (_attack < card._attack)
+                            {
+                                return true;
+                            }
+                            else
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+        case (DMADesc):
+            if (_defense > card._defense)
+            {
+                return true;
+            }
+            else
+            {
+                if (_defense < card._defense)
+                {
+                    return false;
+                }
+                else
+                {
+                    if (_magic > card._magic)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        if (_magic < card._magic)
+                        {
+                            return false;
+                        }
+                        else
+                        {
+                            if (_attack > card._attack)
+                            {
+                                return true;
+                            }
+                            else
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+        case (DAMAsc):
+            if (_defense < card._defense)
+            {
+                return true;
+            }
+            else
+            {
+                if (_defense > card._defense)
+                {
+                    return false;
+                }
+                else
+                {
+                    if (_attack < card._attack)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        if (_attack > card._attack)
+                        {
+                            return false;
+                        }
+                        else
+                        {
+                            if (_magic < card._magic)
+                            {
+                                return true;
+                            }
+                            else
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+        case (DAMDesc):
+            if (_defense > card._defense)
+            {
+                return true;
+            }
+            else
+            {
+                if (_defense < card._defense)
+                {
+                    return false;
+                }
+                else
+                {
+                    if (_attack > card._attack)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        if (_attack < card._attack)
+                        {
+                            return false;
+                        }
+                        else
+                        {
+                            if (_magic > card._magic)
+                            {
+                                return true;
+                            }
+                            else
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+        case (MDAAsc):
+            if (_magic < card._magic)
+            {
+                return true;
+            }
+            else
+            {
+                if (_magic > card._magic)
+                {
+                    return false;
+                }
+                else
+                {
+                    if (_defense < card._defense)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        if (_defense > card._defense)
+                        {
+                            return false;
+                        }
+                        else
+                        {
+                            if (_attack < card._attack)
+                            {
+                                return true;
+                            }
+                            else
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+        case (MDADesc):
+            if (_magic > card._magic)
+            {
+                return true;
+            }
+            else
+            {
+                if (_magic < card._magic)
+                {
+                    return false;
+                }
+                else
+                {
+                    if (_defense > card._defense)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        if (_defense < card._defense)
+                        {
+                            return false;
+                        }
+                        else
+                        {
+                            if (_attack > card._attack)
+                            {
+                                return true;
+                            }
+                            else
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+        case (MADAsc):
+            if (_magic < card._magic)
+            {
+                return true;
+            }
+            else
+            {
+                if (_magic > card._magic)
+                {
+                    return false;
+                }
+                else
+                {
+                    if (_attack < card._attack)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        if (_attack > card._attack)
+                        {
+                            return false;
+                        }
+                        else
+                        {
+                            if (_defense < card._defense)
+                            {
+                                return true;
+                            }
+                            else
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+        case (MADDesc):
+            if (_magic > card._magic)
+            {
+                return true;
+            }
+            else
+            {
+                if (_magic < card._magic)
+                {
+                    return false;
+                }
+                else
+                {
+                    if (_attack > card._attack)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        if (_attack < card._attack)
+                        {
+                            return false;
+                        }
+                        else
+                        {
+                            if (_defense > card._defense)
+                            {
+                                return true;
+                            }
+                            else
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+        case (ADMAsc):
+            if (_attack < card._attack)
+            {
+                return true;
+            }
+            else
+            {
+                if (_attack > card._attack)
+                {
+                    return false;
+                }
+                else
+                {
+                    if (_defense < card._defense)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        if (_defense > card._defense)
+                        {
+                            return false;
+                        }
+                        else
+                        {
+                            if (_magic < card._magic)
+                            {
+                                return true;
+                            }
+                            else
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+        case (ADMDesc):
+            if (_attack > card._attack)
+            {
+                return true;
+            }
+            else
+            {
+                if (_attack < card._attack)
+                {
+                    return false;
+                }
+                else
+                {
+                    if (_defense > card._defense)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        if (_defense < card._defense)
+                        {
+                            return false;
+                        }
+                        else
+                        {
+                            if (_magic > card._magic)
+                            {
+                                return true;
+                            }
+                            else
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+        case (AMDAsc):
+            if (_attack < card._attack)
+            {
+                return true;
+            }
+            else
+            {
+                if (_attack > card._attack)
+                {
+                    return false;
+                }
+                else
+                {
+                    if (_magic < card._magic)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        if (_magic > card._magic)
+                        {
+                            return false;
+                        }
+                        else
+                        {
+                            if (_defense < card._defense)
+                            {
+                                return true;
+                            }
+                            else
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+        default:
+            if (_attack > card._attack)
+            {
+                return true;
+            }
+            else
+            {
+                if (_attack < card._attack)
+                {
+                    return false;
+                }
+                else
+                {
+                    if (_magic > card._magic)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        if (_magic < card._magic)
+                        {
+                            return false;
+                        }
+                        else
+                        {
+                            if (_defense > card._defense)
+                            {
+                                return true;
+                            }
+                            else
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+    }
+}
+
+// Get the score of a Card
+int Card::getScore() const
+{
+    return _attack + _magic + _defense;
+}
+
+// Get Score with coefficient (3 for attack, 2 for magic, 1 for defense)
+int Card::getScoreAMD() const
+{
+    return 3 * _attack + 2 * _magic + _defense;
+}
+
+// Fills a card with the line given in parameter
+void Card::fill(const string &line)
+{
+    // opening the file
+    ifstream file;
+    file.open("C:/Users/pierr/OneDrive/travail IUT/Project-Card-Game/cards_data.txt");
+
+    if (!file.is_open())
+    {
+        throw out_of_range("Error while opening the file, check if the path specified in Card.cpp is right");
+    }
+    else
+    {
+        string actual_line;
+
+        while (getline(file, actual_line))
+        {
+
+            if (actual_line == line)
+            {
+                stringstream stream(line);
+                string value;
+
+
+                /*
+                 * Reads the first value (name) in the string until the next coma and stocks the first value onside the string "value"
+                 * then uses "istringstream" in order to convert the string into an int then stocking it into the variable "_name".
+                */
+                getline(stream, _name, ',');
+
+
+                /*
+                 * Reads the second value (attack) in the string until the next coma and stocks the second value onside the string "value"
+                 * then uses "istringstream" in order to convert the string into an int then stocking it into the variable "_attack".
+                */
+                getline(stream, value, ',');
+                istringstream(value) >> _attack;
+
+
+                /*
+                 * Reads the third value (defense) in the string until the next coma and stocks the third value onside the string "value"
+                 * then uses "istringstream" in order to convert the string into an int then stocking it into the variable "_defense".
+                */
+                getline(stream, value, ',');
+                istringstream(value) >> _defense;
+
+                /*
+                 * Reads the fourth value (magic) in the string until the next coma and stocks the fourth value onside the string "value"
+                 * then uses "istringstream" in order to convert the string into an int then stocking it into the variable "_magic".
+                */
+                getline(stream, value, ',');
+                istringstream(value) >> _magic;
+
+            }
+
+        }
+    }
+    file.close();
 }
 
 // The following methods are not in the instructions, but we had them because they are useful for tests
@@ -143,84 +667,4 @@ int Card::getDefense() const
 int Card::getMagic() const
 {
     return _magic;
-}
-
-void Card::fill(const string &line) {
-    // opening the file
-    ifstream file;
-    file.open("cards_data.txt");
-
-    if (!file.is_open()) {
-        throw out_of_range("Error while opening the file, check if the path specified in Card.cpp is right");
-    }
-    else {
-        string actual_line;
-
-        while (getline(file, actual_line)) {
-
-            if (actual_line == line) {
-                stringstream stream(line);
-                string value;
-
-
-                /*
-
-                 * Reads the first value (name) in the string until the next coma and stocks the first value onside the string "value"
-                 * then uses "istringstream" in order to convert the string into an int then stocking it into the variable "_name".
-
-                */
-                getline(stream, _name, ',');
-
-
-                /*
-
-                 * Reads the second value (attack) in the string until the next coma and stocks the second value onside the string "value"
-                 * then uses "istringstream" in order to convert the string into an int then stocking it into the variable "_attack".
-
-                */
-                getline(stream, value, ',');
-                istringstream(value) >> _attack;
-
-
-                /*
-
-                 * Reads the third value (defense) in the string until the next coma and stocks the third value onside the string "value"
-                 * then uses "istringstream" in order to convert the string into an int then stocking it into the variable "_defense".
-
-                */
-                getline(stream, value, ',');
-                istringstream(value) >> _defense;
-
-                /*
-
-                 * Reads the fourth value (magic) in the string until the next coma and stocks the fourth value onside the string "value"
-                 * then uses "istringstream" in order to convert the string into an int then stocking it into the variable "_magic".
-
-                */
-                getline(stream, value, ',');
-                istringstream(value) >> _magic;
-
-            }
-
-        }
-    }
-    file.close();
-}
-
-
-
-void Card::changeName(const string &newname) {
-    this->_name = newname;
-}
-
-void Card::changeAttack(int newatt) {
-    this->_attack = newatt;
-}
-
-void Card::changeDefense(int newdef) {
-    this->_defense = newdef;
-}
-
-void Card::changeMagic(int newmag) {
-    this->_magic = newmag;
 }

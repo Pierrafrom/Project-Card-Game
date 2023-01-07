fc8b76278f627c2ea92c8fa7a29adcb13ced9e12
Pierre Fromont Boissel <118841244+Pierrafrom@users.noreply.github.com>
2023-01-08 00:31:31 +0100

    Update test.h

--- test.h
+++ test.h
@@ -6,33 +6,59 @@
 #define PROJECT_CARD_GAME_TESTS_H
 
 void testAll();
+
 //################# Card ####################################################
 void testCardConstructors();
+
 void testDisplayLongMethod();
+
 void testDisplayShortMethod();
+
 void testPhysicalDamageMethod();
+
 void testMagicalDamage();
+
 void testCardEqualityOperator();
+
+void testCompareCardMethod();
+
 //################# Player ##################################################
 void testConstructorsPlayer();
+
 void testGettersMethods();
+
 void testDisplayMethod();
+
 void testInitMethod();
+
 void testEnoughCardsMethod();
+
 void testEnoughPrestigeMethod();
+
 void testNextCardMethod();
+
 void testPlaysACardMethod();
+
 void testShuffleMethod();
+
 //################ Game ###################################################
 void testEndedMethod();
+
 void testConstructorsGame();
+
 void testIsGameFinishedMethod();
+
 void testPlaysARoundMethod();
+
 void testWinnerMethod();
+
 void testFillReserve();
+
 // ################ Function ##############################################
 void testSortVectorOfCard();
+
 void testVectorOfRanks();
+
 void testFillDeckWithCriteria();
 
 #endif //PROJECT_CARD_GAME_TESTS_H
